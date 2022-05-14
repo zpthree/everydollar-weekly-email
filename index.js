efc34439da0dd029faaf19b2c8ee9b7198233cf0
Zach Patrick <zach@zachpatrick.com>
2022-05-14 03:16:43 +0000

    setting up with test data

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const getData = require('./getData');
+import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+import getData from './testData.js';
 
-console.log(getData());
+dotenv.config();
+
+(async () => {
+  // const url = `${process.env.API_ENDPOINT}`
+  // const options = { headers: { 'napkin-account-api-key': `${process.env.API_KEY}` } };
+  // const response = await fetch(url, options);
+  // const data = await response.json();
+  const data = getData();
+  const sortedData = data.budget.slice().sort((a, b) => new Date(a.dueDate) - new Date(b.dueDate))
+  console.log(sortedData);
+})()
